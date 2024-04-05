b157670e55ec274798b929ca86c3a1042c86dc29
AAYAN1234567890 <a.samdani@share.epsb.ca>
2024-04-04 18:38:59 -0600

    working

--- Fighters.java
+++ Fighters.java
@@ -3,6 +3,7 @@ import java.util.Collections;
 import java.util.Random;
 
 public class Fighters {
+private static final int XP = 0;
 private String name;
 private int level;
 private String type;
@@ -37,7 +38,7 @@ return this.type;
 	*/
 
 public void doDamage(double damageAmount) { 
-enemy.health -= damageAmount; //Used ChatGPT to clarify how to subtract health (I thought we had to use setter, but ran into an error as object was inaccessible)
+health -= damageAmount; //Used ChatGPT to clarify how to subtract health (I thought we had to use setter, but ran into an error as object was inaccessible)
 System.out.println("Opponent now has " + health);
 }
 
@@ -46,9 +47,9 @@ return this.health;
 }
 
 
-public void attack(String attackName, double damage) {
-System.out.println("You did " + attackName + ". It did " + damage + ".");
-doDamage(damage);
+public void attack(Fighters opponent, String attackName, double damage) {
+    System.out.println("Opponent's current health is " + opponent.getHealth() + ". You did " + attackName + ". It did " + damage + ".");
+    opponent.doDamage(damage);
 //enemy.setHealth(health - damage);
 //implement means to actually deal damage to opponent - this will apply to all fighters
 //once this is done, then the same principle can be carried to gaining xp and levels
@@ -68,8 +69,30 @@ public void dodge() {
     }
 	}
 
-
-
+	public void gainXP(int xp) {
+		System.out.println(name + " gained " + xp + " XP.");
+		 int XP = xp;
+		// Check if level up
+		checkLevelUp();
+	}
+	
+	private void checkLevelUp() {
+		int xpToNextLevel = calculateXPToNextLevel();
+		if (XP >= xpToNextLevel) {
+			levelUp();
+		}
+	}
+	
+	private void levelUp() {
+		level++;
+		System.out.println(name + " leveled up to level " + level + "!");
+	}
+	
+	private int calculateXPToNextLevel() { //Chatgpt
+		// Calculate XP required to reach the next level based on formula
+		return (level * 50) + 50; //the player needs 100 XP to reach level 2.
+	}
+	
 
 
 	public static void main(String[] args) {
@@ -82,14 +105,16 @@ public void dodge() {
 	*/
 		
 	Fighters kundan = new Fighters("name" , 1 , "agile" , 100.0);
-	kundan.attack("punch", 10);
-	kundan.attack("punch", 10);
-	
-	
-	
 	Fighters enemy = new Fighters("enemy", 1, "agile" , 90.0);
-	
+
+	kundan.attack(enemy, "punch", 10);
+	kundan.attack(enemy, "kick", 15);
+
+	// int xpGained = 50;
+    // kundan.gainXP(xpGained);
+
+	System.out.println("Kundan's health: " + kundan.getHealth());
+    System.out.println("Enemy's health: " + enemy.getHealth());
 	}
 
 }
-
